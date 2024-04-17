9c34c34f37f3591ab25660dfd2917666e67522d6
Asphodelus <1749094641@qq.com>
2024-04-17 23:17:21 +0800

    init

---
+++ src/bin/main.rs
@@ -0,0 +1,39 @@
+use clap::Parser;
+use tracing_subscriber::EnvFilter;
+use raft_demo3::start_example_raft_node;
+
+#[derive(Parser, Clone, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Opt {
+    #[clap(long)]
+    pub id: u64,
+
+    #[clap(long)]
+    pub http_addr: String,
+
+    #[clap(long)]
+    pub rpc_addr: String,
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Setup the logger
+    tracing_subscriber::fmt()
+        .with_target(true)
+        .with_thread_ids(true)
+        .with_level(true)
+        .with_ansi(false)
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    // Parse the parameters passed by arguments.
+    let options = Opt::parse();
+
+    start_example_raft_node(
+        options.id,
+        format!("{}.db", options.rpc_addr),
+        options.http_addr,
+        options.rpc_addr,
+    )
+    .await
+}
